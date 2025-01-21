a8f40c68f196f7e13191fe5b8415362833cc780f
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2025-01-21 14:01:41 +0300

    update listeSuiviFlux.blade.php

--- resources/views/MES/suiviFlux/listeSuiviFlux.blade.php
+++ resources/views/MES/suiviFlux/listeSuiviFlux.blade.php
@@ -4,6 +4,138 @@
     <!-- row -->
     <div class="container-fluid">
         @include('MES.headerMES')
+        <div class="row" style="margin-bottom: -20px;margin-top: -10px;">
+            <div class="col-lg-3 col-sm-4">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #3a7bd5, #3a6073);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                Quantité PO</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">5</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-list"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #4568dc, #b06ab3);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                Quantité Coupe</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">4</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-handshake"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #43cea2, #185a9d);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                Quantité Transferes</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">3</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-check-circle"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #f3904f, #3b4371);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                Commande Annulée</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">2</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-times-circle"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 0;">
+            <div class="col-lg-3 col-sm-4">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #ff6e7f, #556770);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                PROTO</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">6</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-cogs"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #16a085, #f4d03f);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                TDS</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">7</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-file-alt"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #82a382, #000c40);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white" style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">
+                                PPS</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">6</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-box"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card card-small"
+                    style="border-radius: 15px 3px 15px 3px; height: 50px; background: linear-gradient(to right, #667eea, #764ba2);">
+                    <div class="card-body d-flex align-items-center justify-content-between" style="height: 100%;">
+                        <div>
+                            <h3 class="card-title text-white"
+                                style="margin-bottom: 5px; font-size: calc(0.1em + 1vw);">PROD</h3>
+                            <div class="d-inline-block">
+                                <h2 class="text-white" style="font-size: calc(0.5em + 1vw);">7</h2>
+                            </div>
+                        </div>
+                        <span class="display-5" style="font-size: calc(1em + 1vw);"><i class="fa fa-industry"
+                                style="color: white;"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @include('CRM.footer')
