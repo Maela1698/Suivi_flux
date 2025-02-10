4d12dfe5b95272c17c4bfae18208c70c9186cdc9
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2025-02-10 14:06:14 +0300

    affichage kpi calendar ppmeeting

--- resources/views/PLANNING/PPM/calendar/calendarPPM.blade.php
+++ resources/views/PLANNING/PPM/calendar/calendarPPM.blade.php
@@ -14,6 +14,62 @@
                     </form>
                 </div>
                 <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card">
+                                <div class="stat-widget-two card-body">
+                                    <div class="stat-content">
+                                        <div class="stat-text">Taux d'achevement</div>
+                                        <div class="stat-digit"> <i class="fa fa-usd"></i>75%</div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-primary w-75" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card">
+                                <div class="stat-widget-two card-body">
+                                    <div class="stat-content">
+                                        <div class="stat-text">Taux de retard</div>
+                                        <div class="stat-digit"> <i class="fa fa-usd"></i>15%</div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-warning w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card">
+                                <div class="stat-widget-two card-body">
+                                    <div class="stat-content">
+                                        <div class="stat-text">Taux a temps</div>
+                                        <div class="stat-digit"> <i class="fa fa-usd"></i>43%</div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-danger w-65" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /# card -->
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card">
+                                <div class="stat-widget-two card-body">
+                                    <div class="stat-content">
+                                        <div class="stat-text">Taux d'absence</div>
+                                        <div class="stat-digit"> <i class="fa fa-usd"></i>56%</div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-success w-85" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /# column -->
+                    </div>
                     <div id="calendar"></div>
                 </div>
             </div>
