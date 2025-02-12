a7f09055d0ed79fdd23471255bdb818b44f2ef9a
miharizo17 <kantomiharizo@gmail.com>
2025-02-12 11:32:45 +0300

    Update maela_temp.sql

--- bdd/suivi_flux/maela_temp.sql
+++ bdd/suivi_flux/maela_temp.sql
@@ -1,30 +1,37 @@
 CREATE OR REPLACE VIEW public.v_ppmeeting
 AS SELECT vd.id,
+    vd.id_tiers,
     vd.nom_modele,
     vd.nomtier,
+    vd.id_etat,
+    vd.etat,
     vd.qte_commande_provisoire,
     vd.types_valeur_ajout,
     d.tissus,
     d.accy,
     d.okprod,
     m.date AS dateppm,
+    dp.datetrace,
     c.designation,
     dm.date_entree_chaine,
     dm.date_entree_coupe,
     dm.date_entree_finition,
-    sv.ex_factory,
     dm.heure_debut,
-    vd.etat,
-    vd.id_etat,
-    dm.etat AS details_meeting_etat,
-    vdm.photo_commande 
+    sv.ex_factory,
+    dm.effectif_prevu,
+    dm.effectif_reel,
+    dm.id_demande,
+    dm.commentaire,
+    dm.id_chaine,
+    dm.etat AS etat_detailmeeting,
+    dp.etat AS etat_trace,
+    vd.photo_commande
    FROM v_general_final_recap vd
      LEFT JOIN datedisponibiliteforppmeeting d ON vd.id = d.id_demande_client
+     LEFT JOIN dateprevisionfortrace dp ON vd.id = dp.id_demande_client
      LEFT JOIN details_meeting dm ON vd.id = dm.id_demande
      LEFT JOIN meeting m ON dm.id_meeting = m.id
      LEFT JOIN chaine c ON dm.id_chaine = c.id_chaine
      LEFT JOIN ( SELECT DISTINCT ON (v_suivifluxmes.id_demande_client) v_suivifluxmes.id_demande_client,
             v_suivifluxmes.ex_factory
-           FROM v_suivifluxmes) sv ON vd.id = sv.id_demande_client
-    JOIN v_demandeclient vdm ON vd.id = vdm.id
-  WHERE vd.etat = 0 AND vd.id_etat = 2;
+           FROM v_suivifluxmes) sv ON vd.id = sv.id_demande_client;
