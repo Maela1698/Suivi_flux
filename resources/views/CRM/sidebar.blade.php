2fd4d6ad9afa9a112ed8df44600a10e6aa332938
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2025-04-03 11:51:42 +0300

    sideBar

--- resources/views/CRM/sidebar.blade.php
+++ resources/views/CRM/sidebar.blade.php
@@ -3,12 +3,12 @@
     <div class="quixnav-scroll" style="margin-top: 10px;">
         <ul class="metismenu" id="menu">
             <li><a class="has-arrow" href="{{ route('CRM.listeDemande') }}" aria-expanded="false"><i
-                        class="icon icon-single-04"></i><span class="nav-text">CRM</span></a>
+                        class="fas fa-users"></i><span class="nav-text">CRM</span></a>
             </li>
         </ul>
         <ul class="metismenu" id="menu">
-            <li><a class="has-arrow" href="{{ route('CRM.nouvelleBc') }}" aria-expanded="false"><i
-                        class="fas fa-clipboard-list"></i><span class="nav-text">NOUVEAU BC</span></a>
+            <li><a class="has-arrow" href="#" aria-expanded="false"><i
+                        class="fas fa-shirt"></i><span class="nav-text">WMS</span></a>
             </li>
         </ul>
         <ul class="metismenu" id="menu">
@@ -17,10 +17,18 @@
             </li>
         </ul>
         <ul class="metismenu" id="menu">
+            <li><a class="has-arrow" href="{{ route('CRM.nouvelleBc') }}" aria-expanded="false"><i
+                        class="fas fa-clipboard-list"></i><span class="nav-text">NOUVEAU BC</span></a>
+            </li>
+        </ul>
+        <ul class="metismenu" id="menu">
             <li><a class="has-arrow" href="{{ route('PLANNING.micro') }}" aria-expanded="false"><i
                         class="icon-calculator"></i><span class="nav-text">PLANNING</span></a>
             </li>
         </ul>
+        
+        
+        
         <ul class="metismenu" id="menu">
             <li><a class="has-arrow" href="{{ route('TRANSIT.acceuil') }}" aria-expanded="false"><i
                     class="icon-plane"></i><span class="nav-text">TRANSIT</span></a>
